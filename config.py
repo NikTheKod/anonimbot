32fd98c27fceb0ee6dcb0052b28b32273f089021
NikTheKod <151569473+NikTheKod@users.noreply.github.com>
2026-04-27 22:02:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))  # Твой Telegram ID
-CHANNEL_ID = os.getenv("CHANNEL_ID")  # Например: @channel или -1001234567890
+ADMIN_ID = os.getenv("ADMIN_ID")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 
-# Проверка наличия переменных
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не найден в .env")
-if not ADMIN_ID:
-    raise ValueError("ADMIN_ID не найден в .env")
-if not CHANNEL_ID:
-    raise ValueError("CHANNEL_ID не найден в .env")
+# Не проверяем здесь, а вынесем проверку в bot.py
